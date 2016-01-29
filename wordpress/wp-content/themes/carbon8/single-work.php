cdc7463eb795314e275a837158a3f143017caa28
Steve Ferrar <steveferrar@gmail.com>
2016-01-29 14:28:28 +0000

    Work navigation

--- wordpress/wp-content/themes/carbon8/single-work.php
+++ wordpress/wp-content/themes/carbon8/single-work.php
@@ -10,15 +10,27 @@ get_header(); ?>
 			
 			<?php echo get_template_part('part', 'page-content' ); ?>
 			<?php echo get_template_part('part', 'work-images' ); ?>
+
+			<section class="work__navigation">
+
+				<div class="container">
+
+					<div class="row">
+							
+						<div class="col-xs-12 col-sm-6 col-sm-offset-6 col-md-6 col-md-offset-6 col-lg-6">
+						<div class="row">
+							<?php 
+							// previous_post_link('%link', 'Previous Project'); 
+							next_post_link('%link', 'Next Project');
+							?>
+						</div>
+						</div>
 					
-			<div class="col-xs-12 col-sm-6 col-sm-offset-6 col-md-6 col-md-offset-6 col-lg-6 work__navigation">
-			<div class="row">
-				<?php 
-				previous_post_link('%link', 'Previous Project'); 
-				next_post_link('%link', 'Next Project');
-				?>
-			</div>
-			</div>
+					</div>
+
+				</div>
+
+			</section>
 			
 		<?php endwhile; ?>
 		
