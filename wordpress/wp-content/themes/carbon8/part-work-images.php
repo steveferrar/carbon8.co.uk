eb104c845b4b0eab407455d5a01598a27ec0e33a
Steve Ferrar <steveferrar@gmail.com>
2016-01-29 13:43:17 +0000

    Updated image section

--- wordpress/wp-content/themes/carbon8/part-work-images.php
+++ wordpress/wp-content/themes/carbon8/part-work-images.php
@@ -15,41 +15,34 @@ if(get_field('images')):
 			<?php
 			while(has_sub_field('images')):
 			?>
-
-				<?php
-				// Large Image
-				if(get_sub_field('number_of_images') == 'One_Large_Image'):
-				if((get_sub_field('image_1')) && !(get_sub_field('image_2'))):
-				?>
 				
-					<div class="row">
-					
-						<?php
-						// Image 1
-						$image_1 = wp_get_attachment_image_src( get_sub_field('image_1') , 'large-image' );
-						?>			
+				<div class="row">
+
+					<?php
+					// Large Image
+					if(get_sub_field('number_of_images') == 'One_Large_Image'):
+					if((get_sub_field('image_1')) && !(get_sub_field('image_2'))):
+					$image_1 = wp_get_attachment_image_src( get_sub_field('image_1') , 'large-image' );
+					?>
+
 						<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
 						<div class="row">
 							<img src="<?php echo $image_1[0]; ?>" alt="<?php the_title(); ?>" class="full-width-image" data-no-retina>
 						</div>
 						</div>
 						
-					</div>
+					<?php
+					endif;
+					endif;
+					?>
 					
-				<?php
-				endif;
-				endif;
-				?>
-				
-				<?php
-				// Two Small Images
-				if(get_sub_field('number_of_images') == 'Two_Small_Images'):
-				if((get_sub_field('image_1')) && (get_sub_field('image_2'))):
-				$image_1 = wp_get_attachment_image_src( get_sub_field('image_1') , 'medium-image' );
-				$image_2 = wp_get_attachment_image_src( get_sub_field('image_2') , 'medium-image' );
-				?>
-				
-					<div class="row">
+					<?php
+					// Two Small Images
+					if(get_sub_field('number_of_images') == 'Two_Small_Images'):
+					if((get_sub_field('image_1')) && (get_sub_field('image_2'))):
+					$image_1 = wp_get_attachment_image_src( get_sub_field('image_1') , 'medium-image' );
+					$image_2 = wp_get_attachment_image_src( get_sub_field('image_2') , 'medium-image' );
+					?>
 
 						<div class="col-xxs-12 col-xs-6 col-sm-6 col-md-6 col-lg-6">
 						<div class="row">
@@ -62,23 +55,20 @@ if(get_field('images')):
 							<img src="<?php echo $image_2[0]; ?>" alt="<?php the_title(); ?>" class="full-width-image" data-no-retina>
 						</div>
 						</div>
-						
-					</div>
-				
-				<?php
-				endif;
-				endif;
-				?>
-				
-				<?php
-				if(get_sub_field('number_of_images') == 'Tall_Left_Image'):
-				if((get_sub_field('image_1')) && (get_sub_field('image_2')) && (get_sub_field('image_tall'))):
-				$image_1 = wp_get_attachment_image_src( get_sub_field('image_1') , 'medium-image' );
-				$image_2 = wp_get_attachment_image_src( get_sub_field('image_2') , 'medium-image' );
-				$image_tall = wp_get_attachment_image_src( get_sub_field('image_tall') , 'tall_image' );
-				?>
-				
-					<div class="row">
+					
+					<?php
+					endif;
+					endif;
+					?>
+					
+					<?php
+					// Tall Image Left, Two Small Images
+					if(get_sub_field('number_of_images') == 'Tall_Left_Image'):
+					if((get_sub_field('image_1')) && (get_sub_field('image_2')) && (get_sub_field('image_tall'))):
+					$image_1 = wp_get_attachment_image_src( get_sub_field('image_1') , 'medium-image' );
+					$image_2 = wp_get_attachment_image_src( get_sub_field('image_2') , 'medium-image' );
+					$image_tall = wp_get_attachment_image_src( get_sub_field('image_tall') , 'tall_image' );
+					?>
 
 						<div class="col-xxs-12 col-xs-6 col-sm-6 col-md-6 col-lg-6">
 						<div class="row">
@@ -92,23 +82,20 @@ if(get_field('images')):
 							<img src="<?php echo $image_2[0]; ?>" alt="<?php the_title(); ?>" class="full-width-image" data-no-retina>
 						</div>
 						</div>
-						
-					</div>
-				
-				<?php
-				endif;
-				endif;
-				?>
-				
-				<?php
-				if(get_sub_field('number_of_images') == 'Tall_Right_Image'):
-				if((get_sub_field('image_1')) && (get_sub_field('image_2')) && (get_sub_field('image_tall'))):
-				$image_1 = wp_get_attachment_image_src( get_sub_field('image_1') , 'medium-image' );
-				$image_2 = wp_get_attachment_image_src( get_sub_field('image_2') , 'medium-image' );
-				$image_tall = wp_get_attachment_image_src( get_sub_field('image_tall') , 'tall_image' );
-				?>
-				
-					<div class="row">
+					
+					<?php
+					endif;
+					endif;
+					?>
+					
+					<?php
+					// Two Small Images, Tall Image Right
+					if(get_sub_field('number_of_images') == 'Tall_Right_Image'):
+					if((get_sub_field('image_1')) && (get_sub_field('image_2')) && (get_sub_field('image_tall'))):
+					$image_1 = wp_get_attachment_image_src( get_sub_field('image_1') , 'medium-image' );
+					$image_2 = wp_get_attachment_image_src( get_sub_field('image_2') , 'medium-image' );
+					$image_tall = wp_get_attachment_image_src( get_sub_field('image_tall') , 'tall_image' );
+					?>
 
 						<div class="col-xxs-12 col-xs-6 col-sm-6 col-md-6 col-lg-6">
 						<div class="row">
@@ -116,19 +103,19 @@ if(get_field('images')):
 							<img src="<?php echo $image_2[0]; ?>" alt="<?php the_title(); ?>" class="full-width-image" data-no-retina>
 						</div>
 						</div>
-							
+
 						<div class="col-xxs-12 col-xs-6 col-sm-6 col-md-6 col-lg-6">
 						<div class="row">
 							<img src="<?php echo $image_tall[0]; ?>" alt="<?php the_title(); ?>" class="full-width-image" data-no-retina>
 						</div>
 						</div>
+					
+					<?php
+					endif;
+					endif;
+					?>
 						
-					</div>
-				
-				<?php
-				endif;
-				endif;
-				?>
+				</div>
 
 			<?php
 			endwhile;
--- wordpress/wp-content/themes/carbon8/single-work.php
+++ wordpress/wp-content/themes/carbon8/single-work.php
@@ -10,10 +10,15 @@ get_header(); ?>
 			
 			<?php echo get_template_part('part', 'page-content' ); ?>
 			<?php echo get_template_part('part', 'work-images' ); ?>
-			
-						
-			<div class="newer-posts"><?php next_post_link('%link', 'Next'); ?></div>
-			<div class="older-posts"><?php previous_post_link('%link', 'Previous Article'); ?></div>
+					
+			<div class="col-xs-12 col-sm-6 col-sm-offset-6 col-md-6 col-md-offset-6 col-lg-6 work__navigation">
+			<div class="row">
+				<?php 
+				previous_post_link('%link', 'Previous Project'); 
+				next_post_link('%link', 'Next Project');
+				?>
+			</div>
+			</div>
 			
 		<?php endwhile; ?>
 		
